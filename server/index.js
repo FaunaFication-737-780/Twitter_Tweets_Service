428ff0240bb2109c48ca373abe9e7b523626315d
Ray Guo <ma2808203259@hotmail.com>
2021-06-07 17:52:35 +0800

    change the rule to have both biodiversity and australia

--- server/index.js
+++ server/index.js
@@ -7,86 +7,86 @@ const config = require('dotenv').config();
 const TOKEN = process.env.TWITTER_BEARER_TOKEN;
 const PORT = process.env.PORT || 3004;
 const app = express();
-const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
 const request = require('request');
 const server = http.createServer(app);
 const io = socketIo(server);
 
-
-app.use(bodyparser.json())
+app.use(bodyparser.json());
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 // eslint-disable-next-line max-len
-const streamURL = 'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
+const streamURL =
+  'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
 
-const rules = [{ value: 'Biodiversity in Australia' }];
+const rules = [{ value: 'Biodiversity AND Australia' }];
 
 // Get stream rules
 async function getRules() {
-    const response = await needle('get', rulesURL, {
-        headers: {
-            Authorization: `Bearer ${TOKEN}`,
-        },
-    });
-    // console.log(response.body);
-    return response.body;
+  const response = await needle('get', rulesURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
+  // console.log(response.body);
+  return response.body;
 }
 
 // Set stream rules
 async function setRules() {
-    const data = {
-        add: rules,
-    };
+  const data = {
+    add: rules,
+  };
 
-    const response = await needle('post', rulesURL, data, {
-        headers: {
-            'content-type': 'application/json',
-            Authorization: `Bearer ${TOKEN}`,
-        },
-    });
+  const response = await needle('post', rulesURL, data, {
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
 
-    return response.body;
+  return response.body;
 }
 
 // Delete stream rules
 async function deleteRules(rules) {
-    if (!Array.isArray(rules.data)) {
-        return null;
-    }
+  if (!Array.isArray(rules.data)) {
+    return null;
+  }
 
-    const ids = rules.data.map((rule) => rule.id);
+  const ids = rules.data.map((rule) => rule.id);
 
-    const data = {
-        delete: {
-            ids: ids,
-        },
-    };
+  const data = {
+    delete: {
+      ids: ids,
+    },
+  };
 
-    const response = await needle('post', rulesURL, data, {
-        headers: {
-            'content-type': 'application/json',
-            Authorization: `Bearer ${TOKEN}`,
-        },
-    });
+  const response = await needle('post', rulesURL, data, {
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
 
-    return response.body;
+  return response.body;
 }
 
 function streamTweets() {
-    const stream = needle.get(streamURL, {
-        headers: {
-            Authorization: `Bearer ${TOKEN}`,
-        },
-    });
+  const stream = needle.get(streamURL, {
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  });
 
-    stream.on('data', (data) => {
-        try {
-            const json = JSON.parse(data);
-            //console.log(json);
-            //socket.emit('TweetData', json);
-            const headersOpt = {
-                "content-type": "application/json",
-            };
-            /*request({
+  stream.on('data', (data) => {
+    try {
+      const json = JSON.parse(data);
+      //console.log(json);
+      //socket.emit('TweetData', json);
+      const headersOpt = {
+        'content-type': 'application/json',
+      };
+      /*request({
                 method: 'post',
                 url: 'http://localhost:3000/Tweet',
                 form: json,
@@ -96,49 +96,54 @@ function streamTweets() {
                 //Print the Response
                 console.log(body);
             });*/
-            request({
-                method: 'post',
-                url: 'https://whydidyoubreaktoday-sleepy-eland.mybluemix.net/Tweet',
-                form: json,
-                headers: headersOpt,
-                json: true,
-            }, function (error, response, body) {
-                //Print the Response
-                console.log(body);
-            });
-            request({
-                method: 'post',
-                url: 'https://animals-in-australia.us-south.cf.appdomain.cloud/Tweet',
-                form: json,
-                headers: headersOpt,
-                json: true,
-            }, function (error, response, body) {
-                //Print the Response
-                console.log(body);
-            });
-        } catch (error) {}
-    });
+      request(
+        {
+          method: 'post',
+          url: 'https://whydidyoubreaktoday-sleepy-eland.mybluemix.net/Tweet',
+          form: json,
+          headers: headersOpt,
+          json: true,
+        },
+        function (error, response, body) {
+          //Print the Response
+          console.log(body);
+        }
+      );
+      request(
+        {
+          method: 'post',
+          url: 'https://animals-in-australia.us-south.cf.appdomain.cloud/Tweet',
+          form: json,
+          headers: headersOpt,
+          json: true,
+        },
+        function (error, response, body) {
+          //Print the Response
+          console.log(body);
+        }
+      );
+    } catch (error) {}
+  });
 
-    return stream;
+  return stream;
 }
-(async function (){
-    let currentRules;
+(async function () {
+  let currentRules;
 
-    try {
-        //   Get all stream rules
-        currentRules = await getRules();
+  try {
+    //   Get all stream rules
+    currentRules = await getRules();
 
-        // Delete all stream rules
-        await deleteRules(currentRules);
+    // Delete all stream rules
+    await deleteRules(currentRules);
 
-        // Set rules based on array above
-        await setRules();
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-    streamTweets();
+    // Set rules based on array above
+    await setRules();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  streamTweets();
 })();
-
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
